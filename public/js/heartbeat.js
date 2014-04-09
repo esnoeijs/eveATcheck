251b3ed45903b7a2a239a628da29e367c82cb489
Erik Snoeijs <erik@consult2.nl>
2014-04-09 23:04:31 +0200

    * Fixed heartbeat bug with always refreshing on the first call (cherry picked from commit 266f0f3)

--- public/js/heartbeat.js
+++ public/js/heartbeat.js
@@ -6,11 +6,17 @@ var heartbeat = function (url, callback)
     this.callback = callback;
     this.lastupdate = 0;
 
-
     this.trigger = function()
     {
         var self = this;
         $.get(self.url, function(data){
+            // Ignore first time.
+            if (self.lastupdate == 0)
+            {
+                self.lastupdate = data;
+                return;
+            }
+
             if (data > self.lastupdate)
             {
                 self.lastupdate = data;
