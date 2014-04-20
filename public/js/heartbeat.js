2b2f585062fed1da8d4278cf47f0a2840b940076
Erik Snoeijs <erik@consult2.nl>
2014-04-20 16:01:03 +0200

    * Added a check to heartbeat to wait for the previous request to finish before sending another.

--- public/js/heartbeat.js
+++ public/js/heartbeat.js
@@ -5,15 +5,21 @@ var heartbeat = function (url, callback)
     this.url = url;
     this.callback = callback;
     this.lastupdate = 0;
+    this.bussy = false;
 
     this.trigger = function()
     {
         var self = this;
-        $.get(self.url, function(data){
+        console.log('trigger called ' + this.bussy);
+        if (self.bussy) return;
+        self.bussy = true;
+        $.get(self.url, function(data)
+        {
             // Ignore first time.
             if (self.lastupdate == 0)
             {
                 self.lastupdate = data;
+                self.bussy = false;
                 return;
             }
 
@@ -22,6 +28,7 @@ var heartbeat = function (url, callback)
                 self.lastupdate = data;
                 self.callback();
             }
+            self.bussy = false;
         });
     }
 
